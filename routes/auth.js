f2a1d9cd618322c6cab1819225c5bf04174d09a1
Paul Pichon F <paul10_barca@hotmail.com>
2023-06-13 20:26:35 -0600

    Se hacen las validaciones con el CHECK de express validator para asegurarnos que viene en el boy tanto correo como password

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,26 @@
 const { Router } = require('express');
 //validaciones
 const { check } = require('express-validator');
+//validar campos
+const { validarCampos } = require('../middlewares/validar-campos');
+
 //importamos los controladores
 const { login } = require('../controllers/auth');
+
 
 //creamos constante router para asignarle la funcion Router de express
 const router = Router();
 
 //RUTAS
 //GET
-router.post('/login', login);
+router.post('/login', [
+    // VALIDAR QUE VENGA EL CORREO
+    check('correo', 'El correo es obligatorio').isEmail(),
+    // contraseña
+    check('password', 'La contraseña es obligatoria').trim().notEmpty(),
+    //llamar mi custom middleware para mostrar los errores
+    validarCampos
+], login);
 
 
 //exports
